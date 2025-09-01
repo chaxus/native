fcf0c824b22f8e227ffd56378788dbfa2bf80bc0
chaxus <nouo18@163.com>
2025-09-01 23:33:47 +0800

    chore: remove obsolete config files and clean up project structure

---
+++ config/index.ts
@@ -0,0 +1,99 @@
+import { defineConfig, type UserConfigExport } from '@tarojs/cli'
+import path from 'path'
+import os from 'os'
+import devConfig from './dev'
+import prodConfig from './prod'
+
+// https://taro-docs.jd.com/docs/next/config#defineconfig-辅助函数
+export default defineConfig<'vite'>(async (merge, { }) => {
+  const baseConfig: UserConfigExport<'vite'> = {
+    projectName: 'taro-harmony',
+    date: '2025-9-1',
+    designWidth: 750,
+    deviceRatio: {
+      640: 2.34 / 2,
+      750: 1,
+      375: 2,
+      828: 1.81 / 2
+    },
+    sourceRoot: 'src',
+    outputRoot: 'dist',
+    plugins: [
+      "@tarojs/plugin-generator",
+      '@tarojs/plugin-platform-harmony-cpp'
+    ],
+    defineConstants: {
+    },
+    copy: {
+      patterns: [
+      ],
+      options: {
+      }
+    },
+    framework: 'react',
+    compiler: 'vite',
+    mini: {
+      postcss: {
+        pxtransform: {
+          enable: true,
+          config: {
+
+          }
+        },
+        cssModules: {
+          enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+          config: {
+            namingPattern: 'module', // 转换模式，取值为 global/module
+            generateScopedName: '[name]__[local]___[hash:base64:5]'
+          }
+        }
+      },
+    },
+    h5: {
+      publicPath: '/',
+      staticDirectory: 'static',
+
+      miniCssExtractPluginOption: {
+        ignoreOrder: true,
+        filename: 'css/[name].[hash].css',
+        chunkFilename: 'css/[name].[chunkhash].css'
+      },
+      postcss: {
+        autoprefixer: {
+          enable: true,
+          config: {}
+        },
+        cssModules: {
+          enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+          config: {
+            namingPattern: 'module', // 转换模式，取值为 global/module
+            generateScopedName: '[name]__[local]___[hash:base64:5]'
+          }
+        }
+      },
+    },
+    rn: {
+      appName: 'taroDemo',
+      postcss: {
+        cssModules: {
+          enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        }
+      }
+    },
+    harmony: {
+      // compiler: 'vite',
+      projectPath: path.join(os.homedir(), '/Desktop/test/tarooh'),
+      hapName: 'entry',
+    },
+  }
+
+  // 移除可能导致冲突的环境变量设置
+  // process.env.BROWSERSLIST_ENV = process.env.NODE_ENV
+
+  if (process.env.NODE_ENV === 'development') {
+    // 本地开发构建配置（不混淆压缩）
+    return merge({}, baseConfig, devConfig)
+  }
+  // 生产构建配置（默认开启压缩混淆等）
+  return merge({}, baseConfig, prodConfig)
+})
